3a983a514b7bf1e6e3dd91a9332adbdcbf1c5899
EricOstoni <eostoni@student.unipu.hr>
2024-01-10 23:13:04 +0100

    fix error in CategoriesAdapter

--- app/src/main/java/com/example/recipeapp/adapters/CategoriesAdapter.kt
+++ app/src/main/java/com/example/recipeapp/adapters/CategoriesAdapter.kt
@@ -9,18 +9,19 @@ import com.example.recipeapp.databinding.CategoryItemBinding
 import com.example.recipeapp.pojo.Category
 
 
-class CategoriesAdapter : RecyclerView.Adapter<CategoriesAdapter.CategoryViewHolder>() {
+class CategoriesAdapter() : RecyclerView.Adapter<CategoriesAdapter.CategoryViewHolder>() {
 
 
     private var categoriesList = ArrayList<Category>()
 
-    @SuppressLint("NotifyDataSetChanged")
+
     fun setCategoryList(categoriesList: List<Category>) {
         this.categoriesList = categoriesList as ArrayList<Category>
         notifyDataSetChanged()
     }
 
-    inner class CategoryViewHolder(val binding: CategoryItemBinding) : RecyclerView.ViewHolder(binding.root)
+    inner class CategoryViewHolder(val binding: CategoryItemBinding) :
+        RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
         return CategoryViewHolder(
@@ -31,11 +32,12 @@ class CategoriesAdapter : RecyclerView.Adapter<CategoriesAdapter.CategoryViewHol
     }
 
     override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
-        Glide.with(holder.itemView).load(categoriesList[position].strCategoryThumb).into(holder.binding.imgCategory)
+        Glide.with(holder.itemView).load(categoriesList[position].strCategoryThumb)
+            .into(holder.binding.imgCategory)
         holder.binding.tvCategoryName.text = categoriesList[position].strCategory
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return categoriesList.size
     }
 }
